7cf029383ad9248e4b2f592241ec44aed63bd3ea
Om <omawadhoot@outlook.com>
2022-06-05 21:31:18 +0530

    Finding address and value of variable

---
+++ Pointers/addressandvalue.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b;
+    a = 5;
+    b = 10;
+    printf("\n The value of A is %d", a);
+    printf("\n The address of A is %u", &a);
+    printf("\n The value of B is %d", b);
+    printf("\n The address of B is %u", &b);
+    getch();
+}
