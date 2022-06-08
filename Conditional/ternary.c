235670ed65d8369bc795bcaddf0c4818958cb665
Om <omawadhoot@outlook.com>
2022-06-08 10:34:46 +0530

    Ternary Operator

---
+++ Conditional/ternary.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main () {
+
+    int i ;
+    printf("Enter the value of i : ");
+    scanf("%d",&i);
+    i>4?printf("Input value is greater than 4 ") : printf("Input value is less than 4 ");
+    getch();
+
+}
