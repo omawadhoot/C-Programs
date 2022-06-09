aed9c4e2907e160e5f8890e3f0aada96c5585a58
Om <omawadhoot@outlook.com>
2022-06-09 16:53:49 +0530

    Remove Marks&PercentCalculator

--- Array/ADDtwoARRAYS.c
+++ Array/ADDtwoARRAYS.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
-void main()
+int main()
 {
     int i, j;
     int a[3][3];
     int b[3][3];
     int c[3][3];
-    printf("\n Enter element for Array A ");
-    for (i = 0; i < 3; ++i)
+    printf(" \n Enter element for Array A \n ");
+    for (i = 0; i < 3; ++i){
         for (j = 0; j < 3; j++)
-            scanf("%d", &a[i][j]);
-    printf("\n Enter element for Array B ");
-    for (i = 0; i < 3; ++i)
+            {scanf("%d", &a[i][j]);}
+    }
+    printf(" \n Enter element for Array B \n ");
+    for (i = 0; i < 3; ++i){
         for (j = 0; j < 3; ++j)
-            scanf("%d", &b[i][j]);
-    for (i = 0; i < 3; ++i)
+            {scanf("%d", &b[i][j]);}
+    }
+    for (i = 0; i < 3; ++i){
         for (j = 0; j < 3; ++j)
-            c[i][j] = a[i][j] + b[i][j];
-    printf("\n The addition is :");
-    for (i = 0; i < 3; ++i)
+            {c[i][j] = a[i][j] + b[i][j];}
+    }
+    printf("\n The addition is : \n\v");
+    for (i = 0; i < 3; ++i){
         for (j = 0; j < 3; ++j)
-            printf("%d", c[i][j]);
+            {   
+                printf("%d\t", c[i][j]);
+            }
+    
+        printf("\n\v");
+    }
+    return 0 ; 
 }
+
+
